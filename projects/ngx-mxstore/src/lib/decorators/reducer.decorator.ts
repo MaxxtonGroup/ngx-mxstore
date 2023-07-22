58bd37e87fe3c99108948ae0c059a95d090b55b7
W. Hollebrandse <w.hollebrandse@maxxton.com>
2023-07-22 20:06:22 +0200

    Update rxjs Change the way reducers are registered

--- projects/ngx-mxstore/src/lib/decorators/reducer.decorator.ts
+++ projects/ngx-mxstore/src/lib/decorators/reducer.decorator.ts
@@ -2,8 +2,9 @@ import {StoreService} from "../store.service";
 
 export function Reducer(action: any ): PropertyDecorator {
   return ( target: Object, propertyKey: string | symbol ) => {
+    const targetStoreService = target.constructor as unknown as StoreService<any>;
     // register the action handler to the target (handling this action is done by the store service).
-    if ((target as StoreService<any>).actionHandlers && (target as StoreService<any>).actionHandlers?.[action.ACTION_TYPE]) {
+    if (targetStoreService.actionHandlers && targetStoreService.actionHandlers?.[action.ACTION_TYPE]) {
       throw new Error(`It is not possible to have multiple REDUCERS for the same action in the same service.
       action: ${action.ACTION_TYPE}, reducer: ${target.constructor.name}.${Object(propertyKey)}`);
     }
